b441796429a7e77c1c09e7925dafda10024be366
presto <yoohan95@gmail.com>
2018-10-16 00:33:54 +0900

    [추가] 멀티태스킹 화면에서 로고 띄우기

--- RespectU/Supporting Files/AppDelegate.swift
+++ RespectU/Supporting Files/AppDelegate.swift
@@ -76,25 +76,51 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        let launchScreen = UIView(frame: UIScreen.main.bounds)
+        launchScreen.tag = 100
+        guard let window = window else { return }
+        launchScreen.backgroundColor = .white
+        launchScreen.alpha = 0
+        window.addSubview(launchScreen)
+        launchScreen.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            launchScreen.topAnchor.constraint(equalTo: window.topAnchor),
+            launchScreen.leadingAnchor.constraint(equalTo: window.leadingAnchor),
+            launchScreen.trailingAnchor.constraint(equalTo: window.trailingAnchor),
+            launchScreen.bottomAnchor.constraint(equalTo: window.bottomAnchor)
+            ])
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "respectuColor"))
+        launchScreen.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.heightAnchor.constraint(equalToConstant: 100),
+            imageView.widthAnchor.constraint(equalToConstant: 100),
+            imageView.centerXAnchor.constraint(equalTo: launchScreen.centerXAnchor),
+            imageView.centerYAnchor.constraint(lessThanOrEqualTo: launchScreen.centerYAnchor)
+            ])
+        UIView.animate(withDuration: 0.2) {
+            launchScreen.alpha = 1
+        }
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        let view = window?.viewWithTag(100)
+        UIView.animate(withDuration: 0.2, animations: {
+            view?.alpha = 0
+        }, completion: { _ in
+            view?.removeFromSuperview()
+        })
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        // Saves changes in the application's managed object context before the application terminates.
+        
     }
 }

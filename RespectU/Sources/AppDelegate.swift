ec1727a79ddb5fff34dc9d375d75639218060569
Presto <yoohan95@gmail.com>
2019-03-08 13:38:51 +0900

    xunique

---
+++ RespectU/Sources/AppDelegate.swift
@@ -0,0 +1,88 @@
+//
+//  AppDelegate.swift
+//  RespectU
+//
+//  Created by Presto on 2017. 9. 26..
+//  Copyright © 2017년 Presto. All rights reserved.
+//
+
+import UIKit
+
+import RealmSwift
+import SwiftKeychainWrapper
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    var config = Realm.Configuration()
+    if let fileURL = config.fileURL {
+      config.fileURL = fileURL.deletingLastPathComponent().appendingPathComponent("new.realm")
+      config.schemaVersion = 1
+      config.migrationBlock = { migration, oldSchemaVersion in
+        if oldSchemaVersion < 1 {
+          
+        }
+      }
+      Realm.Configuration.defaultConfiguration = config
+    }
+    //개발용
+    //        KeychainWrapper.standard.set("", forKey: "id")
+    //        do {
+    //            try FileManager.default.removeItem(at: Realm.Configuration.defaultConfiguration.fileURL!)
+    //        } catch {
+    //            print(error.localizedDescription)
+    //        }
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.tintColor = .main
+    let id = KeychainWrapper.standard.string(forKey: "id") ?? ""
+    if !id.isEmpty {
+      if TipInfo.fetch().count != 0 {
+        let controller = UIViewController.instantiate(storyboard: "Performance", identifier: "PerformanceNavigationController")
+        window?.rootViewController = controller
+      } else {
+        UserDefaults.standard.set(450, forKey: "bpm")
+        let controller = UIViewController.instantiate(storyboard: "Init", identifier: InitViewController.classNameToString)
+        window?.rootViewController = controller
+      }
+    } else {
+      let controller = UIViewController.instantiate(storyboard: "SignIn", identifier: "SignNavigationController")
+      window?.rootViewController = controller
+    }
+    window?.makeKeyAndVisible()
+    if UserDefaults.standard.double(forKey: "bpm") == 0 {
+      UserDefaults.standard.set(450, forKey: "bpm")
+    }
+    if let favoriteButton = UserDefaults.standard.string(forKey: "favoriteButton") {
+      UserDefaults.standard.set(favoriteButton.lowercased(), forKey: "favoriteButton")
+    }
+    // 전역 프로퍼티 설정
+    let navigationBar = UINavigationBar.appearance()
+    navigationBar.setBackgroundImage(UIImage(), for: .default)
+    navigationBar.shadowImage = UIImage()
+    navigationBar.backgroundColor = .clear
+    navigationBar.isTranslucent = true
+    let tableView = UITableView.appearance()
+    tableView.backgroundColor = .white
+    tableView.separatorColor = .lightGray
+    tableView.separatorInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+    tableView.layer.borderColor = UIColor.main.cgColor
+    tableView.layer.borderWidth = 3
+    tableView.layer.cornerRadius = 10
+    tableView.tableFooterView = UIView()
+    return true
+  }
+  
+  func applicationWillResignActive(_ application: UIApplication) { }
+  
+  func applicationDidEnterBackground(_ application: UIApplication) { }
+  
+  func applicationWillEnterForeground(_ application: UIApplication) { }
+  
+  func applicationDidBecomeActive(_ application: UIApplication) { }
+  
+  func applicationWillTerminate(_ application: UIApplication) { }
+}
--- RespectU/Sources/Views/SignInTextField.swift
+++ RespectU/Sources/Views/SignInTextField.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SignInTextField: UITextField {
+final class SignInTextField: UITextField {
   
   override init(frame: CGRect) {
     super.init(frame: frame)
